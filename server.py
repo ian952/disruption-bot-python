5b5fd39c5d987d6ef520fa17f188be85094660ea
ian952 <ianhu952@hotmail.com>
2017-11-05 00:56:53 -0700

    add logging

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import traceback
 from flask import Flask, request
 
 # pylint: disable=C0103
@@ -21,3 +22,31 @@ def interactive():
     Any interactions with message buttons, menus, or dialogs
     '''
     pass
+
+@app.after_request
+def after_request(response):
+    # This IF avoids the duplication of registry in the log,
+    # since that 500 is already logged via @app.errorhandler.
+    if response.status_code != 500:
+        app.logger.info(
+            '%s %s %s %s\nheader:\n%sdata:\n%s',
+            response.status,
+            request.method,
+            request.scheme,
+            request.full_path,
+            request.headers,
+            request.data)
+    return response
+
+@app.errorhandler(Exception)
+def exceptions(e):
+    tb = traceback.format_exc()
+    app.logger.error(
+        '%s %s %s\nheader:\n%sdata:\n%s\n\n%s',
+        request.method,
+        request.scheme,
+        request.full_path,
+        request.headers,
+        request.data,
+        tb)
+    return "Internal Server Error", 500
