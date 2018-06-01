58a1af3265450296f77b257d070d469b4e7c57b1
Ian Hu <ianhu952@hotmail.com>
2018-06-01 13:33:53 -0400

    Strip whitespace

--- handlers/events.py
+++ handlers/events.py
@@ -38,7 +38,7 @@ class History(object):
 
     def get_resp_to_last_msg(self, channel):
         if len(self.history[channel]) > 0:
-            last_msg = self.history[channel][-1]['text'].lower()
+            last_msg = self.history[channel][-1]['text'].lower().strip()
             for k in self.LAST_MSG_RESP:
                 if k == last_msg:
                     return self.LAST_MSG_RESP[k]
